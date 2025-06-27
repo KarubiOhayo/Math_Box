ea16adb2a6c350d7304a041f1111a1e56766cd25
KGS <testEmail@gmail.com>
2025-06-27 18:04:38 +0900

    Add get_Sum function

--- calculator.py
+++ calculator.py
@@ -14,6 +14,7 @@ def divide_new(a, b):
 def getMedian(a, b):
   return (a+b)/2
 
+<<<<<<< HEAD
 def getRemainder(a, b):
   return a//b
 
@@ -25,3 +26,6 @@ def get_Abs(num):
 
 def getPercent(a, b):
   return (a/b) * 100
+
+def getSum_ver1(n):
+  return n(n+1)/2
